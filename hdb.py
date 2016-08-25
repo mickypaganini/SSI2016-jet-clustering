78d2d3c2fce50c6b429d956d9a45bc35fbad76ab
Michela Paganini <micky.91@hotmail.com>
2016-08-25 11:13:48 -0700

    Add HDBSCAN, WardHierarchical, modify AF

--- hdb.py
+++ hdb.py
@@ -1,8 +1,6 @@
 print(__doc__)
 
-from sklearn.cluster import AffinityPropagation
 from sklearn import metrics
-from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 from read_data import read_data
 from sklearn.preprocessing import StandardScaler
@@ -11,8 +9,10 @@ from matplotlib.colors import LogNorm
 from itertools import cycle
 import os
 
-def af(txtfile, event, weighted, noise):
-	run = 'af_10jets_overlap'
+from sklearn.neighbors import DistanceMetric
+
+def hdb(txtfile, event, weighted, noise):
+	run = '10jets_overlap'
 	##############################################################################
 	# Read in sample data
 	X_all, e_all = read_data(txtfile)
@@ -31,7 +31,7 @@ def af(txtfile, event, weighted, noise):
 	# Upweight events with high energy
 	if weighted:
 		# Weight by E/E_noise (actually that would be too high)
-		NOISE_LVL = 0.1 # GeV
+		NOISE_LVL = 0.08 # GeV
 		X_rep = []
 		e_rep = []
 		for p, energy in zip(X, e):
@@ -42,8 +42,12 @@ def af(txtfile, event, weighted, noise):
 			e_rep.extend([energy] * (n_replications))
 		X = np.array(X_rep)
 		e = np.array(e_rep)
+		# Set other params
+		#min_cluster_size=1250
+		min_cluster_size=2000
 		folder = 'weighted'
 	else: # no weighting
+		min_cluster_size=5
 		folder = 'unweighted'
 
 	# Sort output into correct directory
@@ -56,58 +60,62 @@ def af(txtfile, event, weighted, noise):
 		os.makedirs(folder)
 
 	##############################################################################
-	# Compute Affinity Propagation
-	# af = AffinityPropagation(preference=preference).fit(X)
-	# af = AffinityPropagation(preference=((e**2) - np.mean(e**2))).fit(X)
-	#preference = e**2 - np.percentile(e**2, 99.5)
-	#preference = (1 / (1 + np.exp(-(e - e.mean())))) - 1
-	scaler = StandardScaler()
-	scaled_e = scaler.fit_transform(e)
-	preference = scaled_e - 100 # for 2 overlapping jets to give us 1 cluster
-	#preference = scaled_e**6 - 60
-	af = AffinityPropagation(preference=preference).fit(X)
+	# Compute HDBSCAN
+	import hdbscan
+	# from hdbscan.dist_metrics import WMinkowskiDistance
+	# # MINKOWSKI = DistanceMetric.get_metric('wminkowski', p=2, w=e)
+	# MINKOWSKI = WMinkowskiDistance(p=2, w=e)
+
+	# def minkowski(X, Y=None):
+	# 	try:
+	# 		return MINKOWSKI.pairwise(X, Y)
+	# 	except ValueError:
+	# 		return MINKOWSKI.pairwise(X.reshape(1, -1), Y.reshape(1, -1) if Y is not None else None)
+
+	clusterer = hdbscan.HDBSCAN(
+		min_cluster_size=min_cluster_size, 
+		gen_min_span_tree=True,
+		allow_single_cluster=True,
+		#min_samples = 10
+		)
+	clusterer.fit(X)
 
 	##############################################################################
 	# Output cluster info
-	cluster_centers_indices = af.cluster_centers_indices_
-	cluster_centers = af.cluster_centers_
-	labels = af.labels_
-	n_clusters_ = len(cluster_centers_indices)
-	print('Estimated number of clusters: %d' % n_clusters_)
-	for l in range(n_clusters_):
-		#cme = sum(e_unweighted[labels == n])
+	labels = clusterer.labels_
+	n_clusters_ = len(set(labels))
+	if -1 in set(labels): # '-1' is just the group of unclustered objects
+		n_real_clusters = n_clusters_ - 1 
+	else:
+		n_real_clusters = n_clusters_
+	print('Estimated number of clusters: %d' % (n_real_clusters))
+	# Find center of mass energy and position for cluster
+	cluster_centers = []
+	cluster_energies = []
+	for l in set(labels): # !! WARNING !!: these should be computed with unweighted quantities
+		#_e = np.unique(e[labels == l])
+		#_X = np.unique(np.array([(x, y) for x, y in zip(X[labels == l, 0], X[labels == l, 1])]))
 		b = np.array(list(set(zip(X[labels == l, 0], X[labels == l, 1], e[labels == l]))))
 		_X = b[:, :2]
 		_e = b[:, -1].ravel()
 		cme = sum(_e)
 		cm = np.sum(_X * _e[:, np.newaxis], axis=0) / cme
-		print 'Cluster {}: CM = {}, Center = {}, Energy = {}'.format(l, cm, cluster_centers[l], cme)
+		#cme = sum(e[labels == l])
+		#cm = np.sum(X[labels == l] * e[labels == l][:, np.newaxis], axis=0) / cme
+		print 'Cluster {}: Center = {}, Energy = {}'.format(l, cm, cme)
+		cluster_centers.append(cm)
+		cluster_energies.append(cme)
 	
 	##############################################################################
+	# Produce HDBSCAN-specific plots
+	#plot_hdb(clusterer, folder)
+
+	##############################################################################
 	# Plot result
+	# -- display event
 	plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_centers, folder)
-	# # -- display event
-	# binx = np.linspace(-3.0, 3.0, 61)
-	# biny = np.linspace(-3.1, 3.1, 63)
-	# if readin:
-	# 	plt.hist2d(X[:, 0], X[:, 1], weights=e.ravel(), bins=(binx, biny), cmap='rainbow', norm=LogNorm())
-	# 	cb = plt.colorbar()
-	# 	cb.set_label('Energy (GeV)')
-	# plt.xlabel(r'$\eta$')
-	# plt.ylabel(r'$\phi$')
-	# # -- display clustering features
-	# for k in range(n_clusters_):
-	# 	class_members = (labels == k)# & (e > 1)
-	# 	cluster_center = cluster_centers[k]
-	# 	if not readin:
-	# 		plt.plot(X[class_members, 0], X[class_members, 1], 'k.', alpha=0.2)
-	# 	plt.plot(cluster_center[0], cluster_center[1], 'ko', markerfacecolor='None', #markeredgecolor='k', 
-	# 					 markersize=14)
-	# 	for x in X[class_members]:
-	# 		plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
-	# plt.title('Estimated number of clusters: %d' % n_clusters_)
-	# plt.savefig(os.path.join(folder, 'etaphi.pdf'))
-	# plt.show()
+
+# ---------------------------------------------------------------------
 
 def plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_centers, folder):
 	binx = np.linspace(-3.0, 3.0, 61)
@@ -130,14 +138,29 @@ def plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_
 		else:
 			plt.plot(cluster_center[0], cluster_center[1], 'ko', markerfacecolor='None', #markeredgecolor='k', 
 							 markersize=14)
-		cme = sum(e_unweighted[labels == k])
-		if cme > 1:
-			for x in X[class_members]:
-				plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
+		for x in X[class_members]:
+			plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
 
 	plt.savefig(os.path.join(folder, 'etaphi.pdf'))
 	plt.show()
 
+# ---------------------------------------------------------------------
+
+def plot_hdb(clusterer, folder):
+	clusterer.minimum_spanning_tree_.plot(edge_cmap='rainbow', 
+                                      edge_alpha=0.6, 
+                                      node_size=40, 
+                                      edge_linewidth=2)
+	plt.savefig(os.path.join(folder, 'minimum_spanning_tree.jpg'))
+	plt.clf()
+	# clusterer.single_linkage_tree_.plot(cmap='winter')
+	# plt.savefig(os.path.join(folder, 'single_linkage_tree.pdf'))
+	# plt.clf()
+	clusterer.condensed_tree_.plot(select_clusters=True, cmap='winter')
+	plt.savefig(os.path.join(folder, 'condensed_tree.pdf'))
+	plt.clf()
+
+# ---------------------------------------------------------------------
 
 if __name__ == '__main__':
 	import sys
@@ -151,4 +174,5 @@ if __name__ == '__main__':
 	parser.add_argument('--noise', help="boolean that determines whether to add noise", default=False, action='store_true')
 	args = parser.parse_args()
 
-	sys.exit(af(args.txtfile, args.event, args.weighted, args.noise))
+	sys.exit(hdb(args.txtfile, args.event, args.weighted, args.noise))
+	
--- ward.py
+++ ward.py
@@ -1,8 +1,7 @@
+
 print(__doc__)
 
-from sklearn.cluster import AffinityPropagation
 from sklearn import metrics
-from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 from read_data import read_data
 from sklearn.preprocessing import StandardScaler
@@ -11,8 +10,10 @@ from matplotlib.colors import LogNorm
 from itertools import cycle
 import os
 
-def af(txtfile, event, weighted, noise):
-	run = 'af_10jets_overlap'
+from sklearn.neighbors import DistanceMetric
+
+def ward(txtfile, event, weighted, noise):
+	run = 'ward'
 	##############################################################################
 	# Read in sample data
 	X_all, e_all = read_data(txtfile)
@@ -31,7 +32,7 @@ def af(txtfile, event, weighted, noise):
 	# Upweight events with high energy
 	if weighted:
 		# Weight by E/E_noise (actually that would be too high)
-		NOISE_LVL = 0.1 # GeV
+		NOISE_LVL = 0.08 # GeV
 		X_rep = []
 		e_rep = []
 		for p, energy in zip(X, e):
@@ -44,6 +45,7 @@ def af(txtfile, event, weighted, noise):
 		e = np.array(e_rep)
 		folder = 'weighted'
 	else: # no weighting
+		min_cluster_size=5
 		folder = 'unweighted'
 
 	# Sort output into correct directory
@@ -56,58 +58,48 @@ def af(txtfile, event, weighted, noise):
 		os.makedirs(folder)
 
 	##############################################################################
-	# Compute Affinity Propagation
-	# af = AffinityPropagation(preference=preference).fit(X)
-	# af = AffinityPropagation(preference=((e**2) - np.mean(e**2))).fit(X)
-	#preference = e**2 - np.percentile(e**2, 99.5)
-	#preference = (1 / (1 + np.exp(-(e - e.mean())))) - 1
-	scaler = StandardScaler()
-	scaled_e = scaler.fit_transform(e)
-	preference = scaled_e - 100 # for 2 overlapping jets to give us 1 cluster
-	#preference = scaled_e**6 - 60
-	af = AffinityPropagation(preference=preference).fit(X)
+	# Compute Ward
+	from sklearn.cluster import AgglomerativeClustering
 
+	#for linkage in ('ward', 'average', 'complete'):
+	linkage = 'ward'
+	print linkage
+	clusterer = AgglomerativeClustering(linkage=linkage, n_clusters=2)
+	clusterer.fit(X)
+ 
+ 	print 'done'
 	##############################################################################
 	# Output cluster info
-	cluster_centers_indices = af.cluster_centers_indices_
-	cluster_centers = af.cluster_centers_
-	labels = af.labels_
-	n_clusters_ = len(cluster_centers_indices)
-	print('Estimated number of clusters: %d' % n_clusters_)
-	for l in range(n_clusters_):
-		#cme = sum(e_unweighted[labels == n])
+	labels = clusterer.labels_
+	n_clusters_ = len(set(labels))
+	if -1 in set(labels): # '-1' is just the group of unclustered objects
+		n_real_clusters = n_clusters_ - 1 
+	else:
+		n_real_clusters = n_clusters_
+	print('Estimated number of clusters: %d' % (n_real_clusters))
+	# Find center of mass energy and position for cluster
+	cluster_centers = []
+	cluster_energies = []
+	for l in set(labels): # !! WARNING !!: these should be computed with unweighted quantities
+		#_e = np.unique(e[labels == l])
+		#_X = np.unique(np.array([(x, y) for x, y in zip(X[labels == l, 0], X[labels == l, 1])]))
 		b = np.array(list(set(zip(X[labels == l, 0], X[labels == l, 1], e[labels == l]))))
 		_X = b[:, :2]
 		_e = b[:, -1].ravel()
 		cme = sum(_e)
 		cm = np.sum(_X * _e[:, np.newaxis], axis=0) / cme
-		print 'Cluster {}: CM = {}, Center = {}, Energy = {}'.format(l, cm, cluster_centers[l], cme)
+		#cme = sum(e[labels == l])
+		#cm = np.sum(X[labels == l] * e[labels == l][:, np.newaxis], axis=0) / cme
+		print 'Cluster {}: Center = {}, Energy = {}'.format(l, cm, cme)
+		cluster_centers.append(cm)
+		cluster_energies.append(cme)
 	
 	##############################################################################
 	# Plot result
+	# -- display event
 	plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_centers, folder)
-	# # -- display event
-	# binx = np.linspace(-3.0, 3.0, 61)
-	# biny = np.linspace(-3.1, 3.1, 63)
-	# if readin:
-	# 	plt.hist2d(X[:, 0], X[:, 1], weights=e.ravel(), bins=(binx, biny), cmap='rainbow', norm=LogNorm())
-	# 	cb = plt.colorbar()
-	# 	cb.set_label('Energy (GeV)')
-	# plt.xlabel(r'$\eta$')
-	# plt.ylabel(r'$\phi$')
-	# # -- display clustering features
-	# for k in range(n_clusters_):
-	# 	class_members = (labels == k)# & (e > 1)
-	# 	cluster_center = cluster_centers[k]
-	# 	if not readin:
-	# 		plt.plot(X[class_members, 0], X[class_members, 1], 'k.', alpha=0.2)
-	# 	plt.plot(cluster_center[0], cluster_center[1], 'ko', markerfacecolor='None', #markeredgecolor='k', 
-	# 					 markersize=14)
-	# 	for x in X[class_members]:
-	# 		plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
-	# plt.title('Estimated number of clusters: %d' % n_clusters_)
-	# plt.savefig(os.path.join(folder, 'etaphi.pdf'))
-	# plt.show()
+
+# ---------------------------------------------------------------------
 
 def plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_centers, folder):
 	binx = np.linspace(-3.0, 3.0, 61)
@@ -130,14 +122,13 @@ def plot_eta_phi(X, X_unweighted, e, e_unweighted, n_clusters_, labels, cluster_
 		else:
 			plt.plot(cluster_center[0], cluster_center[1], 'ko', markerfacecolor='None', #markeredgecolor='k', 
 							 markersize=14)
-		cme = sum(e_unweighted[labels == k])
-		if cme > 1:
-			for x in X[class_members]:
-				plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
+		for x in X[class_members]:
+			plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], color='black',alpha=0.2)
 
 	plt.savefig(os.path.join(folder, 'etaphi.pdf'))
 	plt.show()
 
+# ---------------------------------------------------------------------
 
 if __name__ == '__main__':
 	import sys
@@ -151,4 +142,5 @@ if __name__ == '__main__':
 	parser.add_argument('--noise', help="boolean that determines whether to add noise", default=False, action='store_true')
 	args = parser.parse_args()
 
-	sys.exit(af(args.txtfile, args.event, args.weighted, args.noise))
+	sys.exit(ward(args.txtfile, args.event, args.weighted, args.noise))
+	
